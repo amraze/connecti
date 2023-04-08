14dcb74aa5c545672f6ae152e06790694ac8e318
Ahmed Mestiri <ahmed.mestiri@supcom.tn>
2023-04-08 21:03:44 +0100

    add Register Method

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -1,3 +1,40 @@
 import bcrypt from "bcrypt";
 import { Jwt } from "jsonwebtoken";
 import User from "../Models/User.js"
+
+export const register = async (request, response) => {
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picture,
+            friends,
+            location,
+            occupation,
+        } = request.body;
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: passwordHash,
+            picture,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000),
+        });
+        const savedUser = await newUser.save();
+        response.status(201).json(savedUser);
+    } catch (error) {
+        response.status(500).json({
+            error: error.message
+        });
+    };
+};
