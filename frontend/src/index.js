bc0f76dcc3bb57741bda11d18897f597ca6400fb
Ahmed Mestiri <ahmed.mestiri@supcom.tn>
2023-04-12 07:45:31 +0100

    add store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import authReducer from "./state";
+import { Provider } from 'react-redux';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { persistStore, persisReducer, FLUSH, REHIDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persisReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredAction: [FLUSH, REHIDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      }
+    })
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}></PersistGate>
+    </Provider>
     <App />
   </React.StrictMode>
 );
