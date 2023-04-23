47a4b08a7da8c99b413f10002776bc3392b6a2da
Ahmed Mestiri <ahmed.mestiri@supcom.tn>
2023-04-23 07:59:26 +0100

    add form.jsx

--- frontend/src/scenes/loginPage/index.jsx
+++ frontend/src/scenes/loginPage/index.jsx
@@ -1,6 +1,34 @@
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
+import { useNavigate } from "react-router-dom";
+import Form from "./form";
+
 const LoginPage = () => {
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    const theme = useTheme();
+    const navigate = useNavigate();
+
+    const neutralLight = theme.palette.neutral.light;
+    const alt = theme.palette.background.alt;
+
     return (
-        <div>LoginPage</div>
+        <Box>
+            <Box width="100%" backgroundColor={alt} p="1rem 6%" textAlign="center">
+                <Typography
+                    fontWeight="bold"
+                    fontSize="32px"
+                    color={theme.palette.mode === "dark" ? "white" : "black"}
+                    onClick={() => navigate("/home")}>
+                    Connecti
+                </Typography>
+            </Box>
+
+            <Box width={isNonMobileScreens ? "50%" : "93%"} p="2rem" m="2rem auto" borderRadius="1.5rem" backgroundColor={alt}>
+                <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+                    Welcome to Connecti, the next-generation Tunisian Social Media !
+                </Typography>
+                <Form />
+            </Box>
+        </Box >
     )
 }
 
